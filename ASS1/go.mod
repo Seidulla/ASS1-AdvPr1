865ab7eb4b7cd97eb9be271eb8ad58f3f8df3314
unknown <220677@astanait.edu.kz>
2024-04-17 12:02:59 +0500

    logging

--- ASS1/go.mod
+++ ASS1/go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
--- ASS1/main.go
+++ ASS1/main.go
@@ -2,27 +2,48 @@ package main
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"golang.org/x/time/rate"
 )
 
+var limiter = rate.NewLimiter(1, 3) // Rate limit of 1 request per second with a burst of 3 requests
+var log = logrus.New()
+
 func main() {
-	err := checkDBConnection()
-	if err != nil {
-		fmt.Println("Failed to connect to the database:", err)
-		return
-	}
+	// Set Logrus formatter
+	log.SetFormatter(&logrus.JSONFormatter{})
 
+	// Initialize the Gorilla mux router
 	r := mux.NewRouter()
-	r.HandleFunc("/", mainPageHandler)
-	r.HandleFunc("/json", handleJSONRequest)
-	r.HandleFunc("/device", createDeviceHandler).Methods("POST")
-	r.HandleFunc("/device/{id}", getDeviceHandler).Methods("GET")
-	r.HandleFunc("/device/{id}", updateDeviceHandler).Methods("PUT")
-	r.HandleFunc("/device/{id}", deleteDeviceHandler).Methods("DELETE")
 
+	// Handle main page requests
+	r.HandleFunc("/", mainPageHandler)
+
+	// Handle JSON requests
+	r.HandleFunc("/json", handleJSONRequest).Methods("POST")
+
+	// Handle device CRUD operations with rate limiting
+	r.HandleFunc("/device", limitMiddleware(createDeviceHandler)).Methods("POST")
+	r.HandleFunc("/device/{id}", limitMiddleware(getDeviceHandler)).Methods("GET")
+	r.HandleFunc("/device/{id}", limitMiddleware(updateDeviceHandler)).Methods("PUT")
+	r.HandleFunc("/device/{id}", limitMiddleware(deleteDeviceHandler)).Methods("DELETE")
+
+	// Start the HTTP server
 	fmt.Println("Server listening on port 8080")
 	http.ListenAndServe(":8080", r)
+}
+
+// Middleware function for rate limiting
+func limitMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
+			return
+		}
+		next.ServeHTTP(w, r)
+	}
 }
